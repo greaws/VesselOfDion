2af764ef62c7206502660adb8c0ca15e3b136578
Paul <paul.amelingmeyer@web.de>
2025-05-13 21:28:02 +0200

    parrallax script, cell shading

--- Assets/Skripte/Player/BetterJumping.cs
+++ Assets/Skripte/Player/BetterJumping.cs
@@ -15,12 +15,6 @@ public class BetterJumping : MonoBehaviour
 
     void Update()
     {
-        if(rb.linearVelocity.y < 0)
-        {
-            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }else if(rb.linearVelocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-        }
+
     }
 }
